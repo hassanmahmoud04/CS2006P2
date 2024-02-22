457f612fd896c6a7730230b6cd037a1dcc4cf80d
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-02-22 13:04:45 +0000

    add ability to check commutivity using iteration

--- intricate_integers.py
+++ intricate_integers.py
@@ -55,17 +55,13 @@ def iterator_has_commutative_intricate_multiplication(n,alpha):
 #     print("Test passed! property holds if and only if α = n − 1")
 #     return()
 
+
 peculiar_property_results = []
 commutative_property_results = []
 
 for n in range(1, 51):
     peculiar_property_results += [(n, a) for a in range(0, n) if iterator_has_intricate_peculiar_property(n, a)]
     commutative_property_results += [(n, a) for a in range(0, n) if iterator_has_commutative_intricate_multiplication(n, a)]
-    # for a in range(0, n):
-    #     # if iterator_has_intricate_peculiar_property(n, a):
-    #     #     peculiar_property_results.append((n, a))
-    #     if iterator_has_commutative_intricate_multiplication(n, a):
-    #         commutativity_results.append((n, a))
 
 test_peculiar_property(peculiar_property_results)
 
