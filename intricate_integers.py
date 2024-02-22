d268a991f576e872a348f8371aeb87927780f4e6
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-02-22 13:03:01 +0000

    add

--- intricate_integers.py
+++ intricate_integers.py
@@ -28,22 +28,48 @@ def iterator_has_intricate_peculiar_property(n,alpha):
             return False
     return True
 
+# check for all pairs (n, α), where 1 ≤ n ≤ 50 and 0 ≤ α < n, that this property holds if and only if α = n − 1.
+def test_peculiar_property(peculiarity_results):
+    for n, a in peculiarity_results:
+        if a != n-1:
+            print("Test failed: property does not hold for",(n, a))
+            return()
+    print("Test passed! property holds if and only if α = n − 1")
+    return()
 
+# check whether or not x ⊗ y = y ⊗ x  holds for all x ∈ Zn
 def iterator_has_commutative_intricate_multiplication(n,alpha):
-    intricate_integers = IntricateIntegers(n, alpha) 
-    for elem in intricate_integers:
+    intricate_integers = IntricateIntegers(n, alpha)
 
+    for i, elem in enumerate(intricate_integers, 0):
+        for j, elem2 in enumerate(intricate_integers, 1): # Start from x+1 to avoid redundant checks
+            if (elem * elem2).value != (elem2 * elem).value:
+                return False
+    return True
+
+# def test_commutative_property(commutativity_results):
+#     for n, a in commutativity_results:
+#         if 
+#             print("Test failed: property does not hold for",(n, a))
+#             return()
+#     print("Test passed! property holds if and only if α = n − 1")
+#     return()
 
 peculiar_property_results = []
-not_commutativity_results = []
+commutative_property_results = []
 
 for n in range(1, 51):
-    for a in range(0, n):
-        if iterator_has_intricate_peculiar_property(n, a):
-            peculiar_property_results.append((n, a))
-        # if not iterator_has_commutative_intricate_multiplication(n, a):
-        #     commutativity_results.append((n, a))
+    peculiar_property_results += [(n, a) for a in range(0, n) if iterator_has_intricate_peculiar_property(n, a)]
+    commutative_property_results += [(n, a) for a in range(0, n) if iterator_has_commutative_intricate_multiplication(n, a)]
+    # for a in range(0, n):
+    #     # if iterator_has_intricate_peculiar_property(n, a):
+    #     #     peculiar_property_results.append((n, a))
+    #     if iterator_has_commutative_intricate_multiplication(n, a):
+    #         commutativity_results.append((n, a))
 
-print("Pairs (n, alpha) where x ⊗ x = x holds:", peculiar_property_results)
+test_peculiar_property(peculiar_property_results)
+
+
+print("Pairs (n, alpha) where commutativity does not hold:", commutative_property_results)
 
                 
