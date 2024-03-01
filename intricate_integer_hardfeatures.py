9daffdc563aae9818bc9dfc696532d6b4aebfe4e
thequantman <137754020+thequantman@users.noreply.github.com>
2024-03-01 23:11:11 +0000

    Add files via upload
    split code into two parts and made various changes to more closely meet the requirements

--- intricate_integer_hardfeatures.py
+++ intricate_integer_hardfeatures.py
@@ -1,58 +1,85 @@
-from intricate_integer import IntricateInteger
 import math
 
-class IntricateIntegers:
-    def __init__(self, modulus, alpha):
+class IntricateInteger:
+    def __init__(self, obj, modulus, alpha):
+        self.value = obj
         self.modulus = modulus
         self.alpha = alpha
-        self.elements = [IntricateInteger(i, modulus, alpha) for i in range(modulus)]
+        ##self.elements = [IntricateInteger(i, modulus, alpha) for i in range(modulus)]
 
     def __str__(self):
-        return "{"+(", ".join(str(element) for element in self.elements))+"}"
-
-    def __iter__(self):
-        for x in self.elements:
-            yield x
+        ##return "{"+(", ".join(str(element) for element in self.elements))+"}"
+        return str(self.value)+" "+str(self.modulus)+" "+str(self.alpha)
 
     def size(self):
         return self.modulus
+    def __mul__(self,other):
+        if self.modulus != other.modulus:
+            raise IntricateIntegerMismatch
+        if self.alpha != other.alpha:
+            raise IntricateIntegerMismatch
+        obj = (self.value+other.value + self.alpha * math.lcm(self.value,other.value)) % self.modulus
+        return IntricateInteger(obj,self.modulus,self.alpha)
 
-intricate_integers = IntricateIntegers(7, 2)
+class IntricateIntegerIterator:
+    def __init__(self, intricate_instance):
+        self.current = intricate_instance.value  # Start iteration from 0
+        self.end = intricate_instance.modulus  # End of iteration defined by n
+        self.alpha = intricate_instance.alpha
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.end:
+            # Create a new instance of A for the current value
+            instance = IntricateInteger(self.current,self.end,self.alpha)
+            self.current += 1
+            return instance
+        else:
+            raise StopIteration
+
+intricate_integers = IntricateInteger(3, 7, 2)
 print(intricate_integers)
 print(intricate_integers.size())
 
 # check whether or not x ⊗ x = x holds for all x ∈ Zn
 def iterator_has_intricate_peculiar_property(n,alpha):
-    intricate_integers = IntricateIntegers(n, alpha)
-    for elem in intricate_integers:
+    intricate_integers = IntricateInteger(0, n, alpha)
+    iterator = IntricateIntegerIterator(intricate_integers)
+    for elem in iterator:
         if (elem * elem).value != elem.value:
             return False
     return True
 
-# check for all pairs (n, α), where 1 ≤ n ≤ 50 and 0 ≤ α < n, that this property holds if and only if α = n − 1.
-def test_peculiar_property(peculiarity_results):
-    for n, a in peculiarity_results:
-        if a != n-1:
-            print("Test failed: property does not hold for",(n, a))
-            return
-    print("Test passed! property holds if and only if α = n − 1")
 
 # check whether or not x ⊗ y = y ⊗ x  holds for all x ∈ Zn
 def iterator_has_commutative_intricate_multiplication(n,alpha):
-    intricate_integers = IntricateIntegers(n, alpha)
+    intricate_integers = IntricateInteger(0, n, alpha)
+    # Create an iterator using the initial_instance
+    iterator = IntricateIntegerIterator(intricate_integers)
+    ##iterator = IntricateIterator(intricate_integers)
 
-    for i, elem in enumerate(intricate_integers, 0):
-        for j, elem2 in enumerate(intricate_integers, 1): # Start from x+1 to avoid redundant checks
+    # Iterate and print the value of n for each new instance of A
+    for elem in iterator:
+        iteratorb = IntricateIntegerIterator(IntricateInteger(elem.value,n,alpha))
+        for elem2 in iteratorb:
+
+    ##for i, elem in enumerate(intricate_integers, 0):
+        ##for j, elem2 in enumerate(intricate_integers, 1): # Start from x+1 to avoid redundant checks
             if (elem * elem2).value != (elem2 * elem).value:
                 return False
     return True
 
 # New feature: Check associative property
 def has_associative_intricate_multiplication(n, alpha):
-    intricate_integers = IntricateIntegers(n, alpha)
-    for x in intricate_integers:
-        for y in intricate_integers:
-            for z in intricate_integers:
+    ##intricate_integers = IntricateInteger(n, alpha)
+    intricate_integers = IntricateInteger(0, n, alpha)
+    iteratorx = IntricateIntegerIterator(intricate_integers)
+    for x in iteratorx:
+        iteratory = IntricateIntegerIterator(intricate_integers)
+        for y in iteratory:
+            iteratorz = IntricateIntegerIterator(intricate_integers)
+            for z in iteratorz:
                 if not (((x * y) * z).value == (x * (y * z)).value):
                     return False
     return True
@@ -66,20 +93,3 @@ def intricate_roots_of_one(n, alpha):
             roots.append(x)
     return roots
 
-peculiar_property_results = []
-commutative_property_results = []
-
-for n in range(1, 51):
-    peculiar_property_results += [(n, a) for a in range(0, n) if iterator_has_intricate_peculiar_property(n, a)]
-    commutative_property_results += [(n, a) for a in range(0, n) if iterator_has_commutative_intricate_multiplication(n, a)]
-
-test_peculiar_property(peculiar_property_results)
-
-print("Pairs (n, alpha) where commutativity does not hold:", commutative_property_results)
-
-# Testing new features
-print("Testing associative property and intricate roots of one...")
-for n in range(1, 6): # Example range, adjust as needed
-    for alpha in range(0, n):
-        print(f"n={n}, alpha={alpha}, associative: {has_associative_intricate_multiplication(n, alpha)}")
-        print(f"n={n}, alpha={alpha}, roots of one: {intricate_roots_of_one(n, alpha)}")
