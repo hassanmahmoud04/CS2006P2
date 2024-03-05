66ca45ab3ebf32f5b6114387658ffa9240f05d47
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-03-05 14:45:00 +0000

    add tests for commutativity

--- i_integerwith_medium_features.py
+++ i_integerwith_medium_features.py
@@ -15,10 +15,6 @@ def iterator_has_intricate_peculiar_property(n,alpha):
 def iterator_has_associative_intricate_multiplication(n, alpha):
     intricate_integers = IntricateIntegers(n, alpha)
 
-    for x in intricate_integers:
-        for y in intricate_integers:
-            for z in intricate_integers:
-def has_associative_intricate_multiplication_iter(n, alpha):
     for x in IntricateIntegers(n,alpha):
         for y in IntricateIntegers(n,alpha):
             for z in IntricateIntegers(n,alpha):
@@ -29,8 +25,8 @@ def has_associative_intricate_multiplication_iter(n, alpha):
 def iterator_has_commutative_intricate_multiplication(n, alpha):
     intricate_integeres = IntricateIntegers(n, alpha)
 
-    for i, elem in enumerate(intricate_integeres):
-        for j, elem2 in enumerate(intricate_integeres):
+    for i, elem in enumerate(intricate_integeres, 0):
+        for j, elem2 in enumerate(intricate_integeres, 1):
             if not ((elem * elem2).value == (elem2 * elem).value):
                 return False
     return True
@@ -38,7 +34,7 @@ def iterator_has_commutative_intricate_multiplication(n, alpha):
 associativity_results = []
 for n in range(1, 21):
     for alpha in range(n):
-        if has_associative_intricate_multiplication_iter(n, alpha):
+        if iterator_has_associative_intricate_multiplication(n, alpha):
             associativity_results.append((n, alpha))
 
 print("Pairs (n, alpha) where associativity holds:", associativity_results)
@@ -50,7 +46,7 @@ for x in intricate_set:
     print(x)
 
 def compare_times(n, alpha):
-    iterator_assoc = timeit.timeit(lambda: has_associative_intricate_multiplication_iter(n, alpha), number=10000)
+    iterator_assoc = timeit.timeit(lambda: iterator_has_associative_intricate_multiplication(n, alpha), number=10000)
     loop_assoc = timeit.timeit(lambda: has_associative_intricate_multiplication(n, alpha), number=10000)
     print("Time for 10000 tests for iterator associativity: " + str(iterator_assoc))
     print("Time for 10000 tests for for loop associativity: " + str(loop_assoc))
